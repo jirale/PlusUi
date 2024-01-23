dc65e65848110292863a2e8bb5d78170ae08ddb7
jirale <149294364+jirale@users.noreply.github.com>
2024-01-23 12:21:00 +0500

    Add files via upload

---
+++ main2.6.4.js
@@ -0,0 +1,95 @@
+var _0x3a3d = ["https://cdn.jsdelivr.net/gh/ngylduy/blog@d0ca1ff", "https://cdn.jsdelivr.net/gh/tanvir-ul10/Flashes-Pro@main", "", "databaseUrl", "realViews", "fb-db", "length", "apps", ".pu-views", "database", "data-id", "getAttribute", "value", "exists", "val", "true", "abbreviation", "data-text", "abv", "setAttribute", "hidden", "data-add", "set", "once", "BlogID_", "/PostID_", "ref", "#maintainCont", "hdn", "push", "\u004A\u0069\u0072\u0061\u006C\u0065\u002E\u0063\u006F\u006D", "\u0077\u0077\u0077\u002E\u006A\u0069\u0072\u0061\u006C\u0065\u002E\u0063\u006F\u006D", "Licensed to: ", ", Licensed for: ", "log", "key", "license", "/js/CopyPreContent.min.js", "pre-js", "body", "/js/NoInternetWidget.min.js", "noInt-js", "#postToc, #autoToc", "#postBody h1, #postBody h2, #postBody h3, #postBody h4, #postBody h5, #postBody h6", "/js/TableOfContents.min.js", "toc-js", "head", "/css/TableOfContents.min.css", "toc-css", "#postToc", "generateToc", "#postBody", "#autoToc", ".dldCo", "/js/CountdownDownloadBox.min.js", "cdb-js", "/css/CountdownDownloadBox.min.css", "cdb-css", "#safelink-setting", "/js/safelink-setting.js", "safelink-setting-js", "/css/safelink-new.css", "safelink-css", "/js/safelink.js", "safelink-js-2", ".pre pre", "/css/highlight.css", "highlight-css", ".pre.ta pre", "/css/morder-syntax.css", "morder-syntax-css", "propertyID", "analytics", "src", "script", "createElement", "https://www.googletagmanager.com/gtag/js?id=", "appendChild", "dataLayer", "js", "config", "https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js", "fb-app", "https://www.gstatic.com/firebasejs/8.4.2/firebase-database.js", "databaseURL", "initializeApp", ".isBkm", "/js/BookmarkPosts.min.js", "bkm-js", "/css/BookmarkPosts.min.css", "bkm-css", ".topM", "/css/topics-menu.css", "topics-css", "lazy", "loadType", "adsenseAds", "/js/AdsenseAds.min.js", "adstr-js", "defer", "#font-family", "mobileFonts", "fontFamily", "id", "style", "FontsStyle", "textContent", "getElementsByTagName", "scroll", "scrollTop", "documentElement", "Lz_Js", "gLS", "sLS", "Lz_JsM", "#ckWrap", "CookiePolicy", "gC", "/css/CookieConsent.min.css", "ck-css", "/js/CookieConsent.min.js", "ck-js", "div.hl pre", "/lib/highlight-11.5.0.min.js", "hl-js", "highlightElement", "forEach", "#rPst", "/css/RelatedPosts.min.css", "rPst-css", "/js/RelatedPosts.min.js", "rPst-js", "div.pre.pF", "/js/code-box.js", "code-box-js", "/css/code-box.css", "code-box-css", "#aRel", "/js/AutoRelated.min.js", "aRel-js", "#aChp", "/js/AutoChapters.min.js", "aChp-js", "addEventListener", "click", "https://cdn.jsdelivr.net/gh/ngylduy/blog@d0ca1ff/js/AdditionalJavascripts.min.js", "add-js", "#pace-progress", "https://cdn.jsdelivr.net/npm/pace-js@latest/pace.min.js", "pace-js"];
+const baseUrl = _0x3a3d[0];
+const newUrl = _0x3a3d[1];
+function puViews() {
+    if (_0x3a3d[2] != PuSet[_0x3a3d[4]][_0x3a3d[3]] && null != getid(_0x3a3d[5]) && 0 < firebase[_0x3a3d[7]][_0x3a3d[6]]) {
+        for (var _0xac0ex4 = qSell(_0x3a3d[8]), _0xac0ex5 = firebase[_0x3a3d[9]](), _0xac0ex6 = 0; _0xac0ex6 < _0xac0ex4[_0x3a3d[6]]; _0xac0ex6++) {
+            var _0xac0ex7 = _0xac0ex4[_0xac0ex6],
+                _0xac0ex8 = _0xac0ex7[_0x3a3d[11]](_0x3a3d[10]);
+            (_0xac0ex8 = _0xac0ex5[_0x3a3d[26]](_0x3a3d[24] + blogID + _0x3a3d[25] + _0xac0ex8))[_0x3a3d[23]](_0x3a3d[12], function (_0xac0ex5, _0xac0ex6) {
+                return function (_0xac0ex9) {
+                    0 < (_0xac0ex9 = _0xac0ex9[_0x3a3d[13]]() ? _0xac0ex9[_0x3a3d[14]]() : 0) && (_0x3a3d[15] == PuSet[_0x3a3d[4]][_0x3a3d[16]] ? _0xac0ex5[_0x3a3d[19]](_0x3a3d[17], Pu[_0x3a3d[18]](_0xac0ex9)) : _0xac0ex5[_0x3a3d[19]](_0x3a3d[17], _0xac0ex9), remCt(_0xac0ex5, _0x3a3d[20])), _0x3a3d[15] == _0xac0ex5[_0x3a3d[11]](_0x3a3d[21]) && (_0xac0ex5[_0x3a3d[19]](_0x3a3d[21], !1), _0xac0ex9 = parseInt(_0xac0ex9) + 1, _0xac0ex6[_0x3a3d[22]](_0xac0ex9))
+                }
+            }(_0xac0ex7, _0xac0ex8))
+        }
+    }
+}
+function blogAdmin() {
+    null != qSel(_0x3a3d[27]) && addCt(qSel(_0x3a3d[27]), _0x3a3d[28])
+}
+var aryLzJs = [];
+function loadLzJs() {
+    var _0xac0exd, _0xac0ex4;
+    aryLzJs[_0x3a3d[29]](1), Defer(function () {
+        var _0xac0exe = _0x3a3d[30],
+            _0xac0exf = _0x3a3d[31];
+        console[_0x3a3d[34]](_0x3a3d[32] + _0xac0exe + _0x3a3d[33] + _0xac0exf)
+    }, 1e3);
+    null != typeof PuSet[_0x3a3d[36]][_0x3a3d[35]] && (_0x3a3d[15] == isSingleItem && ldJs(baseUrl + _0x3a3d[37], _0x3a3d[38], !0, _0x3a3d[39]), ldJs(baseUrl + _0x3a3d[40], _0x3a3d[41], !0, _0x3a3d[39]), _0x3a3d[15] == isSingleItem && (_0xac0exd = qSell(_0x3a3d[42]), _0xac0ex4 = qSell(_0x3a3d[43]), 0 < _0xac0exd[_0x3a3d[6]] && 0 < _0xac0ex4[_0x3a3d[6]] && ldJs(baseUrl + _0x3a3d[44], _0x3a3d[45], !0, _0x3a3d[46], function () {
+        ldCss(baseUrl + _0x3a3d[47], _0x3a3d[48], function () {
+            null != qSel(_0x3a3d[49]) && new TableOfContents({
+                from: qSel(_0x3a3d[51]),
+                to: qSel(_0x3a3d[49])
+            })[_0x3a3d[50]](), null != qSel(_0x3a3d[52]) && new TableOfContents({
+                from: qSel(_0x3a3d[51]),
+                to: qSel(_0x3a3d[52])
+            })[_0x3a3d[50]]()
+        })
+    })), Defer(function () {
+        function _0xac0ex9() {
+            dataLayer[_0x3a3d[29]](arguments)
+        }
+        var _0xac0ex5;
+        0 < qSell(_0x3a3d[53])[_0x3a3d[6]] && ldJs(baseUrl + _0x3a3d[54], _0x3a3d[55], !0, _0x3a3d[39], function () {
+            ldCss(baseUrl + _0x3a3d[56], _0x3a3d[57])
+        }), null != qSel(_0x3a3d[58]) && ldJs(newUrl + _0x3a3d[59], _0x3a3d[60], !0, _0x3a3d[39], function () {
+            ldCss(newUrl + _0x3a3d[61], _0x3a3d[62]), ldJs(newUrl + _0x3a3d[63], _0x3a3d[64], !0, _0x3a3d[39])
+        }), null != qSel(_0x3a3d[65]) && ldCss(newUrl + _0x3a3d[66], _0x3a3d[67]), null != qSel(_0x3a3d[68]) && ldCss(newUrl + _0x3a3d[69], _0x3a3d[70]), _0x3a3d[2] != PuSet[_0x3a3d[72]][_0x3a3d[71]] && ((_0xac0ex5 = document[_0x3a3d[75]](_0x3a3d[74]))[_0x3a3d[73]] = _0x3a3d[76] + PuSet[_0x3a3d[72]][_0x3a3d[71]], document[_0x3a3d[39]][_0x3a3d[77]](_0xac0ex5), window[_0x3a3d[78]] = window[_0x3a3d[78]] || [], _0xac0ex9(_0x3a3d[79], new Date), _0xac0ex9(_0x3a3d[80], PuSet[_0x3a3d[72]][_0x3a3d[71]])), _0x3a3d[2] != PuSet[_0x3a3d[4]][_0x3a3d[3]] && ldJs(_0x3a3d[81], _0x3a3d[82], !0, _0x3a3d[39], function () {
+            ldJs(_0x3a3d[83], _0x3a3d[5], !0, _0x3a3d[39], function () {
+                var _0xac0exd = qSell(_0x3a3d[8]),
+                    _0xac0ex4 = {};
+                _0xac0ex4[_0x3a3d[84]] = PuSet[_0x3a3d[4]][_0x3a3d[3]], firebase[_0x3a3d[85]](_0xac0ex4);
+                for (var _0xac0ex6 = firebase[_0x3a3d[9]](), _0xac0ex7 = 0; _0xac0ex7 < _0xac0exd[_0x3a3d[6]]; _0xac0ex7++) {
+                    var _0xac0ex8 = _0xac0exd[_0xac0ex7],
+                        _0xac0ex10 = _0xac0ex8[_0x3a3d[11]](_0x3a3d[10]);
+                    (_0xac0ex10 = _0xac0ex6[_0x3a3d[26]](_0x3a3d[24] + blogID + _0x3a3d[25] + _0xac0ex10))[_0x3a3d[23]](_0x3a3d[12], function (_0xac0ex5, _0xac0ex6) {
+                        return function (_0xac0ex9) {
+                            0 < (_0xac0ex9 = _0xac0ex9[_0x3a3d[13]]() ? _0xac0ex9[_0x3a3d[14]]() : 0) && (_0x3a3d[15] == PuSet[_0x3a3d[4]][_0x3a3d[16]] ? _0xac0ex5[_0x3a3d[19]](_0x3a3d[17], Pu[_0x3a3d[18]](_0xac0ex9)) : _0xac0ex5[_0x3a3d[19]](_0x3a3d[17], _0xac0ex9), remCt(_0xac0ex5, _0x3a3d[20])), _0x3a3d[15] == _0xac0ex5[_0x3a3d[11]](_0x3a3d[21]) && (_0xac0ex5[_0x3a3d[19]](_0x3a3d[21], !1), _0xac0ex9 = parseInt(_0xac0ex9) + 1, _0xac0ex6[_0x3a3d[22]](_0xac0ex9))
+                        }
+                    }(_0xac0ex8, _0xac0ex10))
+                }
+            })
+        })
+    }, 100), setTimeout(function () {
+        null != qSel(_0x3a3d[86]) && ldJs(baseUrl + _0x3a3d[87], _0x3a3d[88], !0, _0x3a3d[39], function () {
+            ldCss(baseUrl + _0x3a3d[89], _0x3a3d[90])
+        }), null != qSel(_0x3a3d[91]) && ldCss(newUrl + _0x3a3d[92], _0x3a3d[93])
+    }, 200), _0x3a3d[15] != isError && (_0x3a3d[94] == PuSet[_0x3a3d[96]][_0x3a3d[95]] ? ldJs(baseUrl + _0x3a3d[97], _0x3a3d[98]) : _0x3a3d[99] == PuSet[_0x3a3d[96]][_0x3a3d[95]] && Defer[_0x3a3d[79]](baseUrl + _0x3a3d[97], _0x3a3d[98])), lazyCustomJs())
+}
+var aryLzJsM = [];
+function loadLzJsM() {
+    var _0xac0ex9, _0xac0exd;
+    aryLzJsM[_0x3a3d[29]](1), null != qSel(_0x3a3d[100]) && PuSet[_0x3a3d[102]][_0x3a3d[101]] && ((_0xac0exd = (_0xac0ex9 = document)[_0x3a3d[75]](_0x3a3d[104]))[_0x3a3d[103]] = _0x3a3d[105], _0xac0exd[_0x3a3d[106]] = PuSet[_0x3a3d[102]][_0x3a3d[101]], _0xac0ex9[_0x3a3d[107]](_0x3a3d[46])[0][_0x3a3d[77]](_0xac0exd))
+}
+var lazyJs = !1;
+window[_0x3a3d[143]](_0x3a3d[108], function () {
+    (0 != document[_0x3a3d[110]][_0x3a3d[109]] && !1 === lazyJs || 0 != document[_0x3a3d[39]][_0x3a3d[109]] && !1 === lazyJs) && (0 == aryLzJs[_0x3a3d[6]] && null == Pu[_0x3a3d[112]](_0x3a3d[111]) && (loadLzJs(), Pu[_0x3a3d[113]](_0x3a3d[111], 1)), 0 == aryLzJsM[_0x3a3d[6]] && null == Pu[_0x3a3d[112]](_0x3a3d[114]) && (loadLzJsM(), Pu[_0x3a3d[113]](_0x3a3d[114], 1)), null != qSel(_0x3a3d[115]) && null == Pu[_0x3a3d[117]](_0x3a3d[116]) && Defer(function () {
+        ldCss(baseUrl + _0x3a3d[118], _0x3a3d[119], function () {
+            ldJs(baseUrl + _0x3a3d[120], _0x3a3d[121], !0, _0x3a3d[39])
+        })
+    }, 5e3), 0 < qSell(_0x3a3d[122])[_0x3a3d[6]] && ldJs(baseUrl + _0x3a3d[123], _0x3a3d[124], !0, _0x3a3d[39], function () {
+        qSell(_0x3a3d[122])[_0x3a3d[126]]((_0xac0ex9) => {
+            hljs[_0x3a3d[125]](_0xac0ex9)
+        })
+    }), _0x3a3d[15] == isPost && _0x3a3d[15] != isPrivateBlog && (null != qSel(_0x3a3d[127]) && ldCss(baseUrl + _0x3a3d[128], _0x3a3d[129], function () {
+        ldJs(baseUrl + _0x3a3d[130], _0x3a3d[131], !0, _0x3a3d[39])
+    }), null != qSel(_0x3a3d[132]) && ldJs(newUrl + _0x3a3d[133], _0x3a3d[134], !0, _0x3a3d[39], function () {
+        ldCss(newUrl + _0x3a3d[135], _0x3a3d[136])
+    }), null != qSel(_0x3a3d[137]) && ldJs(baseUrl + _0x3a3d[138], _0x3a3d[139], !0, _0x3a3d[39]), null != qSel(_0x3a3d[140]) && ldJs(baseUrl + _0x3a3d[141], _0x3a3d[142], !0, _0x3a3d[39])), scrollCustomJs(), _0x3a3d[15] != isError && _0x3a3d[108] == PuSet[_0x3a3d[96]][_0x3a3d[95]] && ldJs(baseUrl + _0x3a3d[97], _0x3a3d[98]), lazyJs = !0)
+}, !0), qSel(_0x3a3d[39])[_0x3a3d[143]](_0x3a3d[144], (_0xac0ex9) => {
+    0 == aryLzJs[_0x3a3d[6]] && null == Pu[_0x3a3d[112]](_0x3a3d[111]) && (loadLzJs(), Pu[_0x3a3d[113]](_0x3a3d[111], 1)), 0 == aryLzJsM[_0x3a3d[6]] && null == Pu[_0x3a3d[112]](_0x3a3d[114]) && (loadLzJsM(), Pu[_0x3a3d[113]](_0x3a3d[114], 1))
+}), Pu[_0x3a3d[112]](_0x3a3d[111]) && loadLzJs(), Pu[_0x3a3d[112]](_0x3a3d[114]) && loadLzJsM();
+function lazyCallback() {
+    ldJs(_0x3a3d[145], _0x3a3d[146], !0, _0x3a3d[39]), null != qSel(_0x3a3d[147]) && ldJs(_0x3a3d[148], _0x3a3d[149], !0, _0x3a3d[39])
+}
+Lazy(lazyCallback);
